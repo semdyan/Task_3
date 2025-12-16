40c39766d20d40bddeb75c9288baacf7aa4571ca
Semdyan <nikolay.semdyanov@itqc.ru>
2025-12-16 14:16:16 +0400

    Add allure steps to functions in helpers.py

--- helpers.py
+++ helpers.py
@@ -1,8 +1,11 @@
 import random
+
+import allure
 import requests
 
 from data import Urls
 
+@allure.step('Создание тестового пользователя и возврат данных для авторизации')
 def create_and_return_user():
     email = f'{random.randint(100, 1000000)}@testdomain.com'
     password = f'{random.randint(100000, 1000000)}'
@@ -16,6 +19,7 @@ def create_and_return_user():
     requests.post(url=url, json=payload)
     return payload
 
+@allure.step('Удаление тестового пользователя')
 def login_and_delete_user(payload):
     url = f'{Urls.BASE_URL}{Urls.LOGIN_USER_URL}'
     token = requests.post(url=url, json=payload).json().get('accessToken')
